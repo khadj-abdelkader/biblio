abab2b11b2dc468f73ad6e28002568193fa532cf
Karim HADJ-ABDELKADER <karim-hadj@hotmail.fr>
2022-05-10 17:03:56 +0200

    Connection to MySQL Database

---
+++ src/main/java/com/wf3/biblio/model/User.java
@@ -0,0 +1,91 @@
+package com.wf3.biblio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    @Column(name = "adresse_mail", nullable = false, updatable = true)
+    private String email;
+    private String phone;
+    private Boolean enabled = true;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public Boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+}
