0b26ddc3e44c3b65164c77be6172b73d0048d50a
Karim HADJ-ABDELKADER <karim-hadj@hotmail.fr>
2022-05-10 15:13:18 +0200

    Base commit

---
+++ src/main/java/com/wf3/biblio/controller/HomeController.java
@@ -0,0 +1,20 @@
+﻿package com.wf3.biblio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String index () {
+        return "index";
+    }
+
+    @GetMapping("/myAccount")
+    public String myAccount () {
+        return "myaccount";
+    }
+
+
+}
